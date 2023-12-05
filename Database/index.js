0d002a8ac76e03affac85344fcc14eb05df8ce68
RazerEdge64 <kohirkarshubhang@gmail.com>
2023-12-04 19:48:13 -0500

    a6

--- Database/index.js
+++ Database/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** @format */
 
 import courses from "./courses.json" assert { type: "json" }
@@ -15,3 +16,19 @@ export default {
 	grades,
 	enrollments,
 }
+=======
+import courses from "./courses.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import assignments from "./assignments.json" assert { type: "json" };
+import users from "./users.json" assert { type: "json" };
+import grades from "./grades.json" assert { type: "json" };
+import enrollments from "./enrollments.json" assert { type: "json" };
+export default {
+  courses,
+  modules,
+  assignments,
+  users,
+  grades,
+  enrollments,
+};
+>>>>>>> 3bc1e2c (server commit)
